9bac40080be8cd7531551ea65a6bfee015e1b392
neogrammer <jlhardee1986@gmail.com>
2026-06-26 09:12:50 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,210 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+using namespace std;
+struct v3d
+{
+	float x, y, z;
+};
+
+struct triangle
+{
+	v3d p[3];
+};
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
+struct m4x4
+{
+	float m[4][4] = { 0 };
+};
+
+mesh meshCube;
+m4x4 matProj;
+
+float width, height;
+float aspect;
+float fov;
+float fovRad;
+float zFar;
+float zNear;
+
+
+bool update(float dt_);
+void MultMatVec(v3d& i, v3d& o, m4x4& m);
+
+int main()
+{
+
+	meshCube.tris = {
+		// South
+		{0.f,0.f,0.f, 0.f,1.f,0.f, 1.f,1.f,0.f},
+		{0.f,0.f,0.f, 1.f,1.f,0.f, 1.f,0.f,0.f},
+
+		// East
+		{1.f,0.f,0.f,  1.f,1.f,0.f,  1.f,1.f,1.f},
+		{1.f,0.f,0.f,  1.f,1.f,1.f,  1.f,0.f,1.f},
+
+		// North
+		{1.f,0.f,1.f,  1.f,1.f,1.f,  0.f,1.f,1.f},
+		{1.f,0.f,1.f,  0.f,1.f,1.f,  0.f,0.f,1.f},
+
+
+		// West
+		{0.f,0.f,1.f,  0.f,1.f,1.f,  0.f,1.f,0.f},
+		{0.f,0.f,1.f,  0.f,1.f,0.f,  0.f,0.f,0.f},
+
+		// Top
+		{0.f,1.f,0.f,  0.f,1.f,1.f,  1.f,1.f,1.f},
+		{0.f,1.f,0.f,  1.f,1.f,1.f,  1.f,1.f,0.f},
+	};
+	width = 1600.f;
+	height = 900.f;
+	// Projection Matrix
+	zNear = 0.1f;
+	zFar = 1000.f;
+	fov = 90.f;
+	aspect = height / width;
+	fovRad = 1.f / tanf(fov * 0.5f / 180.f * 3.14159f);
+
+	matProj.m[0][0] = aspect * fovRad;
+	matProj.m[1][1] = fovRad;
+	matProj.m[2][2] = zFar / (zFar - zNear);
+	matProj.m[3][2] = (-zFar * zNear) / (zFar - zNear);
+	matProj.m[2][3] = 1.f;
+	matProj.m[3][3] = 0.f;
+
+	sf::RenderWindow wnd{ sf::VideoMode{{(unsigned)width,(unsigned)height},32U}, "3D Engine"};
+
+
+	float dt = 0.f;
+	const float FPS = 1.f / 60.f;
+	sf::Clock dtClock{};
+
+	vector<sf::VertexArray> outline;
+	outline.push_back(sf::VertexArray{ sf::PrimitiveType::Lines, 2 });
+	outline.push_back(sf::VertexArray{ sf::PrimitiveType::Lines, 2 });
+	outline.push_back(sf::VertexArray{ sf::PrimitiveType::Lines, 2 });
+
+	while (wnd.isOpen())
+	{
+		while (const std::optional event = wnd.pollEvent())
+		{
+			if (auto result = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (result->code == sf::Keyboard::Key::Escape)
+				{
+					wnd.close();
+				}
+			}
+			if (event->is<sf::Event::Closed>())
+			{
+				wnd.close();
+			}
+		}
+
+
+		dt += dtClock.restart().asSeconds();
+		if (dt > 0.05f) { dt = 0.05f; }
+
+		bool repaint = false;
+		while (dt >= FPS)
+		{
+			repaint = true;
+
+			if (!update(FPS))
+			{
+				wnd.close();
+				return 1;
+			}
+		
+			dt -= FPS;
+		}
+
+		if (repaint)
+		{
+			wnd.clear();
+
+			for (const auto& tri : meshCube.tris)
+			{
+				triangle triProjected, triTranslated;
+
+				triTranslated = tri;
+				triTranslated.p[0].z = tri.p[0].z + 3.f;
+				triTranslated.p[1].z = tri.p[1].z + 3.f;
+				triTranslated.p[2].z = tri.p[2].z + 3.f;
+
+				MultMatVec(triTranslated.p[0], triProjected.p[0], matProj);
+				MultMatVec(triTranslated.p[1], triProjected.p[1], matProj);
+				MultMatVec(triTranslated.p[2], triProjected.p[2], matProj);
+
+				triProjected.p[0].x += 1.f;
+				triProjected.p[0].y += 1.f;
+				triProjected.p[1].x += 1.f;
+				triProjected.p[1].y += 1.f;
+				triProjected.p[2].x += 1.f;
+				triProjected.p[2].y += 1.f;
+
+				triProjected.p[0].x *= 0.5f * width;
+				triProjected.p[0].y *= 0.5f * height;
+				triProjected.p[1].x *= 0.5f * width;
+				triProjected.p[1].y *= 0.5f * height;
+				triProjected.p[2].x *= 0.5f * width;
+				triProjected.p[2].y *= 0.5f * height;
+
+				// Define two points
+				sf::Vector2f point1A(triProjected.p[0].x, triProjected.p[0].y);
+				sf::Vector2f point1B(triProjected.p[1].x, triProjected.p[1].y);
+				sf::Vector2f point2A(triProjected.p[1].x, triProjected.p[1].y);
+				sf::Vector2f point2B(triProjected.p[2].x, triProjected.p[2].y);
+				sf::Vector2f point3A(triProjected.p[2].x, triProjected.p[2].y);
+				sf::Vector2f point3B(triProjected.p[0].x, triProjected.p[0].y);
+
+				// Create a VertexArray with 2 vertices
+				outline[0][0].position = point1A;
+				outline[0][0].color = sf::Color::White;
+				outline[0][1].position = point1B;
+				outline[0][1].color = sf::Color::White;
+
+				outline[1][0].position = point2A;
+				outline[1][0].color = sf::Color::White;
+				outline[1][1].position = point2B;
+				outline[1][1].color = sf::Color::White;
+
+				outline[2][0].position = point3A;
+				outline[2][0].color = sf::Color::White;
+				outline[2][1].position = point3B;
+				outline[2][1].color = sf::Color::White;
+
+				wnd.draw(outline[0]);
+				wnd.draw(outline[1]);
+				wnd.draw(outline[2]);
+			}
+
+			wnd.display();
+		}
+	}
+
+
+	return 0;
+}
+
+bool update(float dt_)
+{
+	return true;
+}
+
+void MultMatVec(v3d& i, v3d& o, m4x4& m)
+{
+	o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+	o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+	o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+	float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+	if (w != 0.f)
+	{
+		o.x /= w; o.y /= w; o.z /= w;
+	}
+}
